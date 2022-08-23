63a4b6fd6b9397a080dec07c9c195a52305391c0
seongyonglim <seongyonglim@naver.com>
2022-08-23 16:32:05 +0900

    slideshow add

---
+++ main.py
@@ -0,0 +1,47 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout, QGridLayout, QDesktopWidget
+from PyQt5.QtCore import Qt
+
+from gui.slideShow import SlideShow
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+
+    def initUI(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        s = SlideShow()
+        s.setInterval(1000)
+        s.setFixedWidth(300)
+        s.setFixedHeight(900)
+        s.setFilenames(['default.png', './images/default2.png', './images/default3.png'])
+
+        grid.addWidget(s, 0, 0)
+
+
+        self.setWindowTitle('Smart Kiosk')
+        self.resize(300,900)
+        self.center()
+        self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    ex.show()
+    sys.exit(app.exec_())
+
+
