c17a949073f797a05afe42c61a78b3114361358d
yerin0226 <yerin012000@naver.com>
2022-10-12 09:50:42 +0900

    MemuModify

---
+++ util/ReadDataBase.py
@@ -0,0 +1,28 @@
+import pymysql
+
+class ReadDB :
+    def __init__(self):
+        self.mysqldb = pymysql.connect(
+            user='root',
+            password='1234',
+            host='127.0.0.1',
+            port=3307,
+            db='kiosk_DB',
+            charset='utf8')
+
+        # Dict형식의 CURSOR
+        self.cursor = self.mysqldb.cursor(pymysql.cursors.DictCursor)
+
+
+    def getMenu(self):
+        sql = "SELECT * FROM kiosk_db.product;"
+        self.cursor.execute(query=sql)
+
+        result = self.cursor.fetchall()
+        return result
+
+if __name__ == "__main__" :
+    readDb = ReadDB()
+    result = readDb.getMenu()
+    for data in result:
+        print(data)
