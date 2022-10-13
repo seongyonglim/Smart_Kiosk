2b79ab990022205f05b9465f400184783a40e55a
seongyonglim <seongyonglim@naver.com>
2022-10-14 05:46:35 +0900

    MemuModify

--- gui/MenuView.py
+++ gui/MenuView.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtCore import QSize
+from PyQt5.QtCore import QSize, pyqtSignal, QEvent, QObject
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5 import QtGui
@@ -52,11 +52,14 @@ class MyWindow(QMainWindow, form_class):
         self.createCategoryButton()
         # 카테고리 1 메뉴 로드
         self.createMenu(1)
-        self.addOrder("order_name", 3, 1000)
-        self.addOrder("order_name", 1, 2000)
+        self.addOrder("order_name1", 1, 1000)
+        self.addOrder("order_name2", 2, 2000)
+        self.addOrder("order_name3", 3, 3000)
+        self.addOrder("order_name4", 4, 4000)
+        self.addOrder("order_name5", 5, 6000)
+        self.addOrder("order_name6", 6, 7000)
 
-        self.order_layout_list[0][1].setText("HEllo")
-        self.order_list_layout.removeItem(0)
+
 
     def createCategoryButton(self):
         for cur in range(0,len(self.category_name_list)):
@@ -76,6 +79,7 @@ class MyWindow(QMainWindow, form_class):
         orderHB = QHBoxLayout()
         orderHB.addStretch(8)
 
+        # 주문하기 위젯 생성
         order_no_label      = QLabel(str(len(self.order_layout_list) + 1))
         order_name_label    = QLabel(order_name)
 
@@ -114,6 +118,7 @@ class MyWindow(QMainWindow, form_class):
 
         order_widgets_list.append(order_cancle_btn)
 
+        # 버튼들 이벤트 추가
         order_num_plus_btn.clicked.connect(lambda: self.order_count_plus(order_widgets_list))
         order_num_minus_btn.clicked.connect(lambda: self.order_count_minus(order_widgets_list))
         order_cancle_btn.clicked.connect(lambda: self.order_delete(order_widgets_list))
@@ -122,6 +127,12 @@ class MyWindow(QMainWindow, form_class):
         self.order_list_layout.addLayout(orderHB)
         self.order_layout_list.append(order_widgets_list)
 
+
+        self.order_total_price_cal()
+
+    ##########################################################
+    ##########      주문 리스트의 버튼 시작
+    ##########################################################
     def order_count_plus(self,args):
         order_count_label = args[4]
         order_price_label = args[6]
@@ -134,6 +145,8 @@ class MyWindow(QMainWindow, form_class):
 
         order_count_label.setText(text)
         order_total_label.setText(total)
+
+        self.order_total_price_cal()
 
     def order_count_minus(self,args):
         order_count_label = args[4]
@@ -148,12 +161,30 @@ class MyWindow(QMainWindow, form_class):
 
             order_count_label.setText(text)
             order_total_label.setText(total)
+            self.order_total_price_cal()
 
     def order_delete(self,args):
         order_no_label = args[1]
-        no = str(int(order_no_label.text()) -1)
-        print(no)
-        #self.order_layout_list[no].remove(0)
+        no = int(int(order_no_label.text()) -1)
+
+        self.boxdelete(self.order_layout_list[no][0])
+        del self.order_layout_list[no]
+
+        for i in range(0,len(self.order_layout_list)):
+            self.order_layout_list[i][1].setText(str(i+1))
+        self.order_total_price_cal()
+    # 총 금액 계산
+    def order_total_price_cal(self):
+        total_price = 0
+        for i in range(0, len(self.order_layout_list)):
+            total_price += int(self.order_layout_list[i][7].text())
+        self.total_price_label.setText(str(total_price))
+
+    ##########################################################
+    ##########      주문 리스트의 버튼 끝
+    ##########################################################
+
+
 
     def createMenu(self,category):
         readData = self.readDb.getMenu(category)
@@ -181,44 +212,43 @@ class MyWindow(QMainWindow, form_class):
             self.price_list[index].setText("")
             self.price_list[index].setFont(QtGui.QFont("굴림", 15))
 
-    def test(self):
-        img_obj = QPixmap("../resources/etc/default.jpg")
-        self.image_list[1].setPixmap(img_obj)
-        self.name_list[1].setText("")
-        self.name_list[1].setFont(QtGui.QFont("굴림", 15))
-
-        self.price_list[1].setText("")
-        self.price_list[1].setFont(QtGui.QFont("굴림", 15))
 
 
-    def createMenu2(self):
-        category = 2
-        readData = self.readDb.getMenu(category)
-        print(readData)
-        for index in range(0, len(readData)):
-            img_obj = QPixmap(readData[index]['p_img_url'])
-            # img_obj.scaledToWidth(284)
-            # img_obj.scaledToHeight(177)
-            self.image_list[index].setPixmap(img_obj)
-            self.name_list[index].setText(readData[index]['p_name'])
-            self.name_list[index].setFont(QtGui.QFont("굴림", 15))
+    # 레이아웃 제거 Util
+    def deleteItemsOfLayout(self,layout):
+        if layout is not None:
+            while layout.count():
+                item = layout.takeAt(0)
+                widget = item.widget()
+                if widget is not None:
+                    widget.setParent(None)
+                else:
+                    self.deleteItemsOfLayout(item.layout())
 
-            self.price_list[index].setText(str(readData[index]['p_price']))
-            self.price_list[index].setFont(QtGui.QFont("굴림", 15))
+    def boxdelete(self, box):
+        for i in range(self.order_list_layout.count()):
 
-        for index in range(len(readData) + 1, 9):
-            index = index - 1
-            img_obj = QPixmap("../resources/etc/default.jpg")
-            # img_obj.scaledToWidth(284)
-            # img_obj.scaledToHeight(177)
-            self.image_list[index].setPixmap(img_obj)
-            self.name_list[index].setText("")
-            self.name_list[index].setFont(QtGui.QFont("굴림", 15))
-
-            self.price_list[index].setText("")
-            self.price_list[index].setFont(QtGui.QFont("굴림", 15))
+            layout_item = self.order_list_layout.itemAt(i)
+            if layout_item.layout() == box:
+                self.deleteItemsOfLayout(layout_item.layout())
+                self.order_list_layout.removeItem(layout_item)
+                break
 
 
+    #  위젯 클릭 이벤트 Util
+    def clickable(widget):
+        class Filter(QObject):
+            clicked = pyqtSignal()
+            def eventFilter(self, obj, event):
+                if obj == widget:
+                    if event.type() == QEvent.MouseButtonRelease:
+                        if obj.rect().contains(event.pos()):
+                            self.clicked.emit()
+                            return True
+                return False
+        filter = Filter(widget)
+        widget.installEventFilter(filter)
+        return filter.clicked
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
